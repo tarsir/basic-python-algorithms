2b41661d4e612d2ca64f08a4b282d87d8e386c6e
Stephen Hara <sthara216@gmail.com>
2017-03-26 11:59:46 -0400

    add some more a* functions

--- 02-a-star-pathfinding/grid.py
+++ 02-a-star-pathfinding/grid.py
@@ -3,12 +3,6 @@ grid.py: contains the classes for defining a grid layout
 """
 import math
 
-class Node:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return "({0})".format(self.name)
 
 class Edge:
     def __init__(self, src_node, target_node, cost):
@@ -18,17 +12,23 @@ class Edge:
 
     def __str__(self):
         return "{0} -> {1}: {2}".format(
-           self.src_node,
-           self.target_node,
-           self.cost
+            self.src_node,
+            self.target_node,
+            self.cost
         )
 
 class Grid:
+    """Holds the definition of a grid layout with some helper functions.
+    
+    Attributes:
+        grid_layout: list of lists, representing a 2-dimensional grid of nodes
+        edges: list of edges
+    """
+
     def __init__(self, length, width):
         grid_row = [0] * length
         self.grid_layout = [grid_row] * width
         self.edges = []
-        print(grid_row)
 
     def __str__(self):
         return '\n'.join(
@@ -38,12 +38,14 @@ class Grid:
         )
 
     def is_edge(self, src, target):
+        """Determines if src and target are connected by an edge in that direction."""
         for edge in self.edges:
             if edge.src_node == src and edge.target_node == target:
                 return True
         return False
 
     def get_adjacent_edges(self, node):
+        """Returns the list of edges node is a source of."""
         adjacents = []
         for edge in self.edges:
             if edge.src_node == node:
@@ -51,6 +53,11 @@ class Grid:
         return adjacents
 
 def node_distance(node_one, node_two):
+    """
+    Calculate the straight-line distance between two nodes.
+
+    Acts as the heuristic for this implementation of A*
+    """
     d_x = node_two[0] - node_one[0]
     d_y = node_two[1] - node_one[1]
     return math.sqrt(d_x * d_x + d_y*d_y)
@@ -67,7 +74,16 @@ def create_uniform_edges(grid):
             grid.edges.append(edge_below)
     return grid
 
-grid = Grid(10, 10)
-grid = create_uniform_edges(grid)
-print(grid.get_adjacent_edges((4, 3)))
+def find_path_a_star(grid, start, goal):
+    """
+    Find the shortest path between two nodes with the A* algorithm.
+    """
+    p_queue = [start]
+    while len(p_queue) > 0:
+        pass
+
+
+our_grid = Grid(10, 10)
+our_grid = create_uniform_edges(our_grid)
+print(our_grid.get_adjacent_edges((4, 3)))
 print(node_distance((0, 0), (9, 0)))
