34cda19496b141c9a76c05a879c3aed8541ce468
Stephen Hara <sthara216@gmail.com>
2017-03-26 20:42:15 -0400

    finish merge_sort implementation

--- 01-sorting/sorting.py
+++ 01-sorting/sorting.py
@@ -34,18 +34,28 @@ def merge_sort(numbers):
     if length == 0 or length == 1:
         return numbers
     else:
-        splitting_index = length / 2
+        splitting_index = int(length / 2)
         left_list, right_list = merge_sort(
-            numbers[:splitting_index],
+            numbers[:splitting_index]
+        ), merge_sort(
             numbers[splitting_index:]
         )
         return _merge_lists(left_list, right_list)
 
 def _merge_lists(left_list, right_list):
     left_ptr, right_ptr = 0, 0
+    combined_list = []
     while left_ptr < len(left_list) and right_ptr < len(right_list):
-        pass
+        if left_list[left_ptr] < right_list[right_ptr]:
+            combined_list.append(left_list[left_ptr])
+            left_ptr += 1
+        else:
+            combined_list.append(right_list[right_ptr])
+            right_ptr += 1
+    return combined_list + left_list[left_ptr:] + right_list[right_ptr:]
 
-unsorted_numbers = [9, 7, 5, 6, 1, 4, 2]
+unsorted_numbers = [59, 98, 62, 94, 30, 91, 29, 6, 53, 70, 42, 83, 58, 12, 1, 11, 67, 25, 90, 7, 67, 49, 65, 60, 19, 16, 88, 13, 25, 85, 54, 76, 50, 43, 94, 83, 9, 98, 78, 72, 82, 10, 22, 75, 29, 40, 66, 77, 47, 92, 15, 61, 52, 1, 85, 82, 75, 2, 19, 32, 91, 16, 55, 5, 41, 15, 24, 2, 43,
+49, 10, 90, 80, 11, 90, 25, 59, 37, 75, 33, 13, 48, 85, 84, 36, 22, 31, 83, 63, 96, 67, 89, 21, 100, 43, 94, 83, 7, 46, 65]
 print (bubble_sort(unsorted_numbers))
 print (insertion_sort(unsorted_numbers))
+print (merge_sort(unsorted_numbers))
